db93f888e01767a28522401059b9899e42a42940
sdunlap1 <sdunlap1@mac.com>
2024-10-23 11:44:37 -0700

    Removed relationship between projects and tasks. The should be separate

--- models/index.js
+++ models/index.js
@@ -11,8 +11,8 @@ Task.belongsTo(User, { foreignKey: 'user_id' });  // A Task belongs to a User
 User.hasMany(Project, { foreignKey: "user_id" });  // A User has many Projects
 Project.belongsTo(User, { foreignKey: "user_id" });  // A Project belongs to a User
 
-Project.hasMany(Task, { foreignKey: "project_id" });  // A Project has many Tasks
-Task.belongsTo(Project, { foreignKey: "project_id" });  // A Task belongs to a Project
+// Project.hasMany(Task, { foreignKey: "project_id" });  // A Project has many Tasks
+// Task.belongsTo(Project, { foreignKey: "project_id" });  // A Task belongs to a Project
 
 Category.hasMany(Task, { foreignKey: 'category_id' });  // A Category has many Tasks
 Task.belongsTo(Category, { foreignKey: 'category_id' });  // A Task belongs to a Category
