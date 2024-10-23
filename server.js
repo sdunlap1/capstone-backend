58da07e485093652d5ee1f2e7bc9c042640abb4c
sdunlap1 <sdunlap1@mac.com>
2024-10-23 14:39:42 -0700

    Still trying to get it to work

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = require("./app");
 const { PORT } = require("./config/config");
 const sequelize = require("./config/sequelize");  // Sequelize connection
 
+// Bind the server to 0.0.0.0
+const HOST = '0.0.0.0';
+
 // Sync the database and start the server
 sequelize.sync({ alter: true })
   .then(() => {
@@ -12,7 +15,7 @@ sequelize.sync({ alter: true })
     const HOST = process.env.HOST || 'localhost'; // Use HOST from environment or default to localhost
     const PORT = process.env.PORT || 3001;
     
-    app.listen(PORT, HOST, function () {
+    app.listen(HOST, PORT, function () {
       console.log(`Server started on http://${HOST}:${PORT}`);
     });
   })
