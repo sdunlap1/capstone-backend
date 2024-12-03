245d68e1beb7f9fa370f8519e9997a4ef5dbb188
sdunlap1 <sdunlap1@mac.com>
2024-12-02 22:15:19 -0800

    Added validation checks. Destructured req.body

--- routes/projects.js
+++ routes/projects.js
@@ -4,35 +4,64 @@ const express = require("express");
 const router = new express.Router();
 const Project = require("../models/project");
 const { authenticateJWT } = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const { Op } = require("sequelize");
 
 // Create a new project
-router.post("/", authenticateJWT, async (req, res, next) => {
-  try {
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-    const dueDate = new Date(req.body.due_date);
-
-    const project = await Project.create({
-      name: req.body.name,
-      description: req.body.description,
-      start_date: req.body.start_date, // Include start_date
-      due_date: req.body.due_date, // Include due_date for multi-day projects
-      user_id: req.user.user_id,
-      completed: req.body.completed || false,
-      notified_past_due: dueDate < today,
-    });
-
-    return res.status(201).json({ project });
-  } catch (err) {
-    if (err.name === "SequelizeValidationError") {
-      return res.status(400).json({
-        errors: err.errors.map((e) => ({ message: e.message })),
-      });
+router.post(
+  "/",
+  authenticateJWT,
+  [
+    // Validation checks
+    check("name")
+      .notEmpty()
+      .withMessage("Name is required")
+      .isLength({ max: 255 })
+      .withMessage("Name cannot be longer than 255 characters"),
+    check("description")
+      .optional()
+      .isString()
+      .withMessage("Invalid description"),
+      check("start_date").optional().isString().withMessage("Invalid format"),
+      check("due_date").optional().isString().withMessage("Invalid format"),
+      check("completed").optional().isString().withMessage("Box must be checked"),
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    return next(err); // Pass unexpected errors to error handling middleware
+
+    try {
+      const { name, description, start_date, due_date, completed } = req.body;
+
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      const dueDate = new Date(req.body.due_date);
+      console.log(dueDate);
+
+      const project = await Project.create({
+        name,
+        description: description || null,
+        start_date: start_date || null,
+        due_date: due_date || null,
+        user_id: req.user.user_id,
+        completed: completed || false,
+        notified_past_due: dueDate < today,
+      });
+
+      return res.status(201).json({ project });
+    } catch (err) {
+      if (err.name === "SequelizeValidationError") {
+        return res.status(400).json({
+          errors: err.errors.map((e) => ({ message: e.message })),
+        });
+      }
+      return next(err); // Pass unexpected errors to error handling middleware
+    }
   }
-});
-const { Op } = require("sequelize");
+);
 
 // Get all projects for the authenticated user
 router.get("/", authenticateJWT, async (req, res, next) => {
@@ -83,7 +112,7 @@ router.put("/:project_id", authenticateJWT, async (req, res, next) => {
       } else if (!project.notified_past_due) {
         project.notified_past_due = true;
       }
-      
+
       project.due_date = req.body.due_date;
     }
     if (req.body.completed !== undefined)
